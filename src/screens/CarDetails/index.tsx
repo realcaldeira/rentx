abe87f507012540d2b3ce0028d2f6f8a5562040f
Lucas Caldeira <mrrealcaldeira@gmail.com>
2021-06-04 14:33:55 -0300

    create button

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+import { Accessory } from '../../components/Accessory';
+import { Button } from '../../components/Button';
+
+import speedSvg from '../../assets/speed.svg';
+import accelerationSvg from '../../assets/acceleration.svg';
+import forceSvg from '../../assets/force.svg';
+import gasolineSvg from '../../assets/gasoline.svg';
+import exchangeSvg from '../../assets/exchange.svg';
+import peopleSvg from '../../assets/people.svg';
 
 import {
   Container,
   Header,
-  CarImages
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Acessories,
+  Footer
 } from './styles';
 
 export function CarDetails(){
@@ -17,10 +37,44 @@ export function CarDetails(){
             />
         </Header>
 
-        <CarImages>
+        
           <ImageSlider imagesUrl={['https://automobile-assets.s3.amazonaws.com/automobile/body/audi-rs5-2020-2022-1613028935.6138937.png']}
           />
-        </CarImages>
+
+          <Content>
+            <Details>
+              <Description>
+                <Brand>Lamborghini</Brand>
+                <Name>Huracan</Name>
+              </Description>
+
+              <Rent>
+                <Period>Ao dia</Period>
+                <Price>R$ 580</Price>
+              </Rent>
+            </Details>
+
+            <Acessories>
+              <Accessory name="380Km/h" icon={speedSvg}/>
+              <Accessory name="3.2s" icon={accelerationSvg}/>
+              <Accessory name="800 HP" icon={forceSvg}/>
+              <Accessory name="Gasolina" icon={gasolineSvg}/>
+              <Accessory name="Auto" icon={exchangeSvg}/>
+              <Accessory name="2 pessoas" icon={peopleSvg}/>
+            </Acessories>
+
+            <About>
+              Este é automóvel despostivo. Surgiu do lendário 
+              touro de lide indultado ma praça Real Maestranza de Sevilla. 
+              É um belíssimo carro para quem gosta de acelerar
+            </About>
+          </Content>
+
+          <Footer>
+            <Button title="Confirmar"/>
+          </Footer>
+
+        
     </Container>
    );
 }
--- src/screens/CarDetails/styles.ts
+++ src/screens/CarDetails/styles.ts
@@ -1,4 +1,5 @@
-import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { getBottomSpace, getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
@@ -13,9 +14,85 @@ export const Header = styled.View`
 
   position: absolute;
   margin-top: ${getStatusBarHeight() + 18}px;
-  margin-left: 24px;
+  margin-left: ${RFValue(24)}px;
 `;
 
-export const CarImages = styled.View`
-  margin-top: ${getStatusBarHeight() + 32}px;
+
+export const Content = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    padding: 24,
+    alignItems: 'center'
+  },
+  showsVerticalScrollIndicator: false
+})``;
+
+export const Details = styled.View`
+  width: 100%;
+  
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: ${RFValue(38)}px;
+`;
+
+export const Description = styled.View``;
+
+export const Brand = styled.Text`
+  font-family: ${({ theme })=> theme.fonts.secondary_500};
+  color: ${({ theme })=> theme.colors.text_detail};
+  font-size: ${RFValue(10)}px;
+
+  text-transform: uppercase;
+`;
+
+export const Name = styled.Text`
+  font-family: ${({ theme })=> theme.fonts.secondary_500};
+  color: ${({ theme })=> theme.colors.title};
+  font-size: ${RFValue(25)}px;
+
+`;
+
+export const Rent = styled.View``;
+
+export const Period = styled.Text`
+  font-family: ${({ theme })=> theme.fonts.secondary_500};
+  color: ${({ theme })=> theme.colors.text_detail};
+  font-size: ${RFValue(10)}px;
+
+  text-transform: uppercase;
+`;
+
+export const Price = styled.Text`
+  font-family: ${({ theme })=> theme.fonts.secondary_500};
+  color: ${({ theme })=> theme.colors.main};
+  font-size: ${RFValue(25)}px;
+`;
+
+export const About = styled.Text`
+  font-family: ${({ theme })=> theme.fonts.primary_400};
+  color: ${({ theme })=> theme.colors.text};
+  font-size: ${RFValue(15)}px;
+  text-align: justify;
+  
+  margin-top: ${RFValue(23)}px;
+  line-height: ${RFValue(25)}px;
+`;
+
+export const Acessories = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: ${RFValue(16)}px;
+`;
+
+export const Footer = styled.View`
+  width: 100%;
+  background-color: ${({ theme })=> theme.colors.background_secondary};
+
+  padding: 24px 24px ${getBottomSpace() + 24}px;
 `;
