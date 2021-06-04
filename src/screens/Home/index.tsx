451b5b076c839bb46b593399bc72687984b04e6e
Lucas Caldeira <mrrealcaldeira@gmail.com>
2021-06-04 04:40:51 -0300

    cards

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,6 +4,8 @@ import { RFValue } from 'react-native-responsive-fontsize'
 
 import Logo from '../../assets/logo.svg';
 
+import { Car } from '../../components/Car';
+
 import {
   Container,
   Header,
@@ -12,6 +14,26 @@ import {
 } from './styles';
 
 export function Home(){
+  const carDataOne = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent:{
+        period: 'AO DIA',
+        price: 120,
+    },
+    thumbnail: 'https://automobile-assets.s3.amazonaws.com/automobile/body/audi-rs5-2020-2022-1613028935.6138937.png'
+  }
+  const carDataTwo = {
+    brand: 'Porche',
+    name: 'Panamera',
+    rent:{
+        period: 'AO DIA',
+        price: 340,
+    },
+    thumbnail: 'https://www.pngkit.com/png/full/237-2375888_porsche-panamera-s.png'
+  }
+
+
   return (
     <Container>
       <StatusBar 
@@ -30,6 +52,10 @@ export function Home(){
           </TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carDataOne}/>
+      <Car data={carDataTwo}/>
+
     </Container>
    );
 }
