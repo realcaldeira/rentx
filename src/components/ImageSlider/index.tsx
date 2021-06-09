532a299086c04a993aef7103c63d0bddb55f2617
Lucas Caldeira <mrrealcaldeira@gmail.com>
2021-06-09 10:40:48 -0300

    imageIndex

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, { useState, useRef} from 'react';
+import { FlatList, ViewToken } from 'react-native';
 import {
   Container,
   ImageIndexes,
@@ -12,22 +12,49 @@ interface Props {
     imagesUrl: string[];
 }
 
+interface ChangeImageProps {
+    viewableItems: ViewToken[];
+    changed: ViewToken[];
+}
+
 export function ImageSlider({ imagesUrl }: Props){
-  return (
+  const [imageIndex, setImageIndex]= useState(0);
+  
+  const indexChanged = useRef ((info: ChangeImageProps)=> {
+    const index = info.viewableItems[0].index!;
+    setImageIndex(index);
+  });
+  
+    return (
     <Container>
         <ImageIndexes>
-            <ImageIndex active={true}/>
-            <ImageIndex active={false}/>
-            <ImageIndex active={false}/>
-            <ImageIndex active={false}/>
+            {
+                imagesUrl.map((_, index) =>(
+                    <ImageIndex
+                        key={String(index)} 
+                        active={index === imageIndex}
+                    />
+                ))
+            }
+            
         </ImageIndexes>
 
-        <CarImageWrapper>
-            <CarImage 
-                source={{uri: imagesUrl[0]}}
-                resizeMode="contain"
+        
+            <FlatList 
+                data={imagesUrl}
+                keyExtractor={key => key}
+                renderItem={({ item })=> (
+                    <CarImageWrapper>
+                        <CarImage 
+                            source={{uri: item}}
+                            resizeMode="contain"
+                            />
+                    </CarImageWrapper>
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                onViewableItemsChanged={indexChanged.current}
             />
-        </CarImageWrapper>
     </Container>
    );
 }
