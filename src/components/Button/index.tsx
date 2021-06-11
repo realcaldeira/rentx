3bc14354e050c48cc4e0d404ffc7cb79c50315de
Lucas Caldeira <mrrealcaldeira@gmail.com>
2021-06-11 17:55:07 -0300

    initial SignIn

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -12,6 +12,7 @@ interface Props {
     onPress: ()=> void;
     enabled?: boolean;
     loading?: boolean;
+    light?: boolean;
 }
 
 export function Button({
@@ -20,6 +21,7 @@ export function Button({
     onPress,
     enabled = true,
     loading = false,
+    light = false,
 }:Props){
 
   const theme = useTheme();
@@ -32,7 +34,7 @@ export function Button({
       >
         { loading  
         ? <ActivityIndicator color={theme.colors.shape}/>
-        : <Title>{title}</Title>
+        : <Title light={light}>{title}</Title>
         }
     </Container>
    );
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -8,6 +8,7 @@ import { SchedulingDetails } from '../screens/SchedulingDetails';
 import { SchedulingComplete } from '../screens/SchedulingComplete';
 import { MyCars } from '../screens/MyCars';
 import { Splash } from '../screens/Splash';
+import { SignIn } from '../screens/SignIn';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -15,11 +16,11 @@ export function StackRoutes(){
     return(
         <Navigator 
             headerMode="none" 
-            initialRouteName="Splash"
+            initialRouteName="SignIn"
         >
             <Screen 
-                name="Splash"
-                component={Splash}
+                name="SignIn"
+                component={SignIn}
             />
             
             <Screen 
